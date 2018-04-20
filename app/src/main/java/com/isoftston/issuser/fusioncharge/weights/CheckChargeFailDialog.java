21e36bd1aa3d31e8898856564fd09b7e9a80b6bd
yzh <356589990@qq.com>
2018-04-20 18:18:46 +0800

    111

---
+++ app/src/main/java/com/isoftston/issuser/fusioncharge/weights/CheckChargeFailDialog.java
@@ -0,0 +1,41 @@
+package com.isoftston.issuser.fusioncharge.weights;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.isoftston.issuser.fusioncharge.R;
+
+/**
+ * Created by issuser on 2018/4/20.
+ */
+
+public class CheckChargeFailDialog extends Dialog{
+
+    private Context context;
+    private ImageView iv_cancel;
+
+    public CheckChargeFailDialog(@NonNull Context context) {
+        super(context, R.style.MyDialog1);
+        this.context=context;
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_check_charge_fail);
+        iv_cancel=findViewById(R.id.iv_cancel);
+
+        iv_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+    }
+}
