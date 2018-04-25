37e73f5969a37429b6506970ab7b3c808ccf2013
TokyoAndroid <vicwudi@126.com>
2018-04-26 01:01:02 +0800

    zhangwei:增加扫码页面1

---
+++ app/src/main/java/com/isoftston/issuser/fusioncharge/views/ChargeInputNumberActivity.java
@@ -0,0 +1,26 @@
+package com.isoftston.issuser.fusioncharge.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.corelibs.base.BaseActivity;
+import com.corelibs.base.BasePresenter;
+import com.isoftston.issuser.fusioncharge.R;
+
+public class ChargeInputNumberActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_charge_input_number;
+    }
+
+    @Override
+    protected void init(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected BasePresenter createPresenter() {
+        return null;
+    }
+}
