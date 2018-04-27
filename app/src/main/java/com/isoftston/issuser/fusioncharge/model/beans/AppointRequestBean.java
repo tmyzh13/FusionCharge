6623b2c07fb14fb88962f41439ec353fa307a6be
vicwudi@126.com <Zl344738736>
2018-04-27 19:09:35 +0800

    zhangwei: 调试通过充电站详情。

--- app/src/main/java/com/isoftston/issuser/fusioncharge/model/beans/AppointRequestBean.java
+++ app/src/main/java/com/isoftston/issuser/fusioncharge/model/beans/AppointRequestBean.java
@@ -7,7 +7,6 @@ package com.isoftston.issuser.fusioncharge.model.beans;
  */
 
 public class AppointRequestBean {
-    private String reserveId;//预约ID
     private String appUserId;//APP用户ID
     private String gunCode;//枪编号
     private String chargingPileId;//充电桩ID
@@ -17,4 +16,59 @@ public class AppointRequestBean {
     private String reserveEndTime;
     private String reserveDuration;
 
+    public String getAppUserId() {
+        return appUserId;
+    }
+
+    public void setAppUserId(String appUserId) {
+        this.appUserId = appUserId;
+    }
+
+    public String getGunCode() {
+        return gunCode;
+    }
+
+    public void setGunCode(String gunCode) {
+        this.gunCode = gunCode;
+    }
+
+    public String getChargingPileId() {
+        return chargingPileId;
+    }
+
+    public void setChargingPileId(String chargingPileId) {
+        this.chargingPileId = chargingPileId;
+    }
+
+    public String getChargingPileName() {
+        return chargingPileName;
+    }
+
+    public void setChargingPileName(String chargingPileName) {
+        this.chargingPileName = chargingPileName;
+    }
+
+    public String getReserveBeginTime() {
+        return reserveBeginTime;
+    }
+
+    public void setReserveBeginTime(String reserveBeginTime) {
+        this.reserveBeginTime = reserveBeginTime;
+    }
+
+    public String getReserveEndTime() {
+        return reserveEndTime;
+    }
+
+    public void setReserveEndTime(String reserveEndTime) {
+        this.reserveEndTime = reserveEndTime;
+    }
+
+    public String getReserveDuration() {
+        return reserveDuration;
+    }
+
+    public void setReserveDuration(String reserveDuration) {
+        this.reserveDuration = reserveDuration;
+    }
 }
